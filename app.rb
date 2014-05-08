8e1d54495383ac548105d1e2940eef4e67137635
darinacosta <darinjacosta@gmail.com>
2014-05-07 19:16:15 -0500

    folded Enabler and ImageScraper classes into single class

--- app.rb
+++ app.rb
@@ -12,11 +12,12 @@ end
 
 Session = GoogleDrive.login(ENV['GOOGLE_USER'], ENV['GOOGLE_PASS'])
 
+
 require_relative "lib/image_collector.rb"
 
 #paths
 get '/' do
   url='http://railscasts.com/episodes/190-screen-scraping-with-nokogiri'
-  @test=ImageCollector::Enabler.parse(url)
+  @test=ImageCollector::ImageScraper.new.pullpage(url)
   erb :out
 end
--- lib/image_collector.rb
+++ lib/image_collector.rb
@@ -1,36 +1,25 @@
 module ImageCollector
 
 
-  class Enabler
+  class ImageScraper
 
-    def initialize(url)
+    def initialize
       @agent = Mechanize.new { |agent| agent.user_agent_alias = "Mac Safari" }
-      @url=url
       @worksheet = Session.spreadsheet_by_url("https://docs.google.com/a/searchinfluence.com/spreadsheets/d/1Xo98mtLrWfy4fpL3-60lOLKX7wmxRK92o14cE8hAEZQ/edit#gid=0").worksheets[0]
-    end
-
-    def self.parse(url)
-      page = ImageScraper.new(url).pullpage(url)
-      return page
     end 
-
-  end
-
-
-  class ImageScraper < Enabler
 
     def pullpage(url)
         html = @agent.get(url).body
         page = Nokogiri::HTML(html)
         page_images=[]
-        root_url=@url.split('/')
+        root_url=url.split('/')
         root_url=root_url[0]+'//'+root_url[1]+root_url[2]
         page.css('img').each do |image|
           path=image['src']
           absolute_uri = URI.join(root_url, path).to_s
           page_images.push(absolute_uri)
         end
-        return ImageWriter.new(url).image_loop(page_images)
+        return ImageWriter.new.image_loop(page_images)
       rescue Mechanize::ResponseCodeError => e  
         page_images="error"
         return page
@@ -40,7 +29,7 @@ module ImageCollector
 
 
 
-  class ImageWriter < Enabler
+  class ImageWriter < ImageScraper
 
     def next_index
       @worksheet.num_rows + 1
